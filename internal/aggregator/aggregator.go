ddae1144868a861c87b9457fecf2921f230fa34a
Hursow <thuanraellombardi@gmail.com>
2025-04-28 20:09:37 -0300

    refactor GetLogsFromSources function to improve log handling and structure

--- internal/aggregator/aggregator.go
+++ internal/aggregator/aggregator.go
@@ -2,23 +2,30 @@ package aggregator
 
 import (
 	config "tidybeaver/internal/config"
-	sources "tidybeaver/internal/sources"
+	source "tidybeaver/internal/sources"
+	storage "tidybeaver/internal/storage"
+	"tidybeaver/pkg/models"
 )
 
+var Logs models.SampleLogs
+
 func GetLogsFromSources(configs config.Configs, userInputConfig config.UserInputConfigurations) {
-	if config.UserInputConfig.UseAPI {
+	if config.UserInputConfig.UseMock {
+		Logs = source.GetLogsFromMock()
+	} else {
+		if config.UserInputConfig.UseAPI {
+		}
+		if config.UserInputConfig.UseDB {
+		}
+		if config.UserInputConfig.UseFS {
+		}
+		if config.UserInputConfig.UseMSVC {
+		}
+		if config.UserInputConfig.UseWin {
+		}
+	}
+}
 
-	}
-	if config.UserInputConfig.UseDB {
-
-	}
-	if config.UserInputConfig.UseFS {
-		sources.GetLogsFromFS()
-	}
-	if config.UserInputConfig.UseMSVC {
-
-	}
-	if config.UserInputConfig.UseWin {
-
-	}
+func WriteLogsToStorage() {
+	storage.WriteLogsToFile(Logs)
 }
