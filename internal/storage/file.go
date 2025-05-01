80abbf46492e1f479909136fc68fc480f1b1a5d2
Hursow <thuanraellombardi@gmail.com>
2025-05-01 15:36:06 -0300

    refactor WriteLogsToFile function to improve logging and ensure JSON output is handled correctly

--- internal/storage/file.go
+++ internal/storage/file.go
@@ -1,14 +1,24 @@
 package storage
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	models "tidybeaver/pkg/models"
 )
 
 func WriteLogsToFile(receivedLogs models.SampleLogs) {
-	fmt.Println("Logs received to be writen to file:")
+	fmt.Println("Logs received to be writen to file (text):")
 	fmt.Println(receivedLogs)
 	receivedLogsByte := fmt.Sprint(receivedLogs)
 	os.WriteFile(("TidyBeaverLogs.txt"), []byte(receivedLogsByte), 0644)
+
+	encodedLogs, err := json.Marshal(receivedLogs)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Logs received to be writen to file (json):")
+	fmt.Println(string(encodedLogs))
+	os.WriteFile(("TidyBeaverLogs.json"), encodedLogs, 0644)
 }
