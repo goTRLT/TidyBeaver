8c033be3ee4ac7778d41b5166433d9301a2c862d
Hursow <thuanraellombardi@gmail.com>
2025-05-02 22:30:49 -0300

    refactor WindowsEventLog struct to change Index and InstanceID types to string for consistency

--- internal/sources/os.go
+++ internal/sources/os.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 	config "tidybeaver/internal/config"
 )
@@ -15,13 +16,15 @@ type WindowsEventLogs struct {
 }
 
 type WindowsEventLog struct {
-	Index      int    `json:"Index"`
+	Index      string `json:"Index"`
 	Time       string `json:"Time"`
 	EntryType  string `json:"EntryType"`
 	Source     string `json:"Source"`
-	InstanceID int    `json:"InstanceID"`
+	InstanceID string `json:"InstanceID"`
 	Message    string `json:"Message"`
 }
+
+var windowsEventLog WindowsEventLog
 
 func GetLogsFromOS() {
 	cmd := exec.Command("powershell", "-Command", "Get-EventLog -LogName Application -Newest "+config.ConfigValues.App.LogAmount+" | Format-Table -AutoSize")
@@ -32,14 +35,25 @@ func GetLogsFromOS() {
 		fmt.Printf("Error running PowerShell command: %v\n", err)
 		return
 	}
-	fmt.Println("Application Event Logs:")
-	fmt.Println(out.String())
+	fmt.Println("Application Event Logs: ", out.String())
 	os.WriteFile("TBEventLogs.txt", out.Bytes(), 0644)
 
 	stringz := out.String()
 	strLines := SplitLines(stringz)
-	fmt.Println("strLines:")
-	fmt.Println(strLines)
+
+	for i := 0; i < len(strLines); i++ {
+
+		regex := regexp.MustCompile(`^.{1}(.{6})`)
+		test := regex.FindString(strLines[i])
+		fmt.Println("test: ", test)
+
+		fmt.Println("strLine: ", strings.Split(strLines[i], strLines[i]))
+		// windowsEventLog.Index =	string(regex.Split(strLines[i], -1))
+		// windowsEventLog.Time = regexp.MustCompile("^.{7}(.{19})")
+		// windowsEventLog.EntryType = regexp.MustCompile("^.{10}(.{5})")
+		// windowsEventLog.Source = regexp.MustCompile("^.{10}(.{5})")
+		// windowsEventLog.Message = regexp.MustCompile("^.{10}(.{5})")
+	}
 
 	// receivedLogs := ""
 	// encodedLogs, err := json.Marshal(receivedLogs)
@@ -57,6 +71,7 @@ func SplitLines(s string) []string {
 	sc := bufio.NewScanner(strings.NewReader(s))
 	for sc.Scan() {
 		lines = append(lines, sc.Text())
+
 	}
 	return lines
 }
