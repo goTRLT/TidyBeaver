9034d72bbc6dadfe91c9e4be8f1f50120e6a343a
Hursow <thuanraellombardi@gmail.com>
2025-05-02 22:30:44 -0300

    refactor GetLogsFromMock and related functions to improve naming and consistency in log generation

--- internal/sources/mock.go
+++ internal/sources/mock.go
@@ -3,7 +3,6 @@ package sources
 import (
 	"fmt"
 	"math/rand"
-	"strconv"
 	config "tidybeaver/internal/config"
 	models "tidybeaver/pkg/models"
 	"time"
@@ -13,9 +12,9 @@ var SampleLogsEntry models.SampleLogs
 var SampleLogEntry models.SampleLog
 
 func GetLogsFromMock() models.SampleLogs {
-	generatedMockLogs := GenerateMockLogs()
-	fmt.Println(generatedMockLogs)
-	return generatedMockLogs
+	generatedSampleLogs := GenerateSampleLogs()
+	fmt.Println(generatedSampleLogs)
+	return generatedSampleLogs
 }
 
 func randomChoice(choices []string) string {
@@ -29,22 +28,19 @@ func randomTime() time.Time {
 	return now.Add(-time.Duration(randomizedOffset) * time.Second)
 }
 
-func selectMessage(selectedLevel string) string {
-	var selectMessage string
-	if selectedLevel == "WARN" || selectedLevel == "ERROR" {
-		selectMessage = randomChoice(models.AvailableErrorMessages)
-	} else if selectedLevel == "INFO" || selectedLevel == "DEBUG" {
-		selectMessage = randomChoice(models.AvailableInfoMessages)
+func selectSampleMessage(sampleLevel string) string {
+	var sampleMessage string
+	if sampleLevel == "WARN" || sampleLevel == "ERROR" {
+		sampleMessage = randomChoice(models.SampleErrorMessages)
+	} else if sampleLevel == "INFO" || sampleLevel == "DEBUG" {
+		sampleMessage = randomChoice(models.SampleInfoMessages)
 	}
-	return selectMessage
+	return sampleMessage
 }
 
-func GenerateMockLogs() models.SampleLogs {
+func GenerateSampleLogs() models.SampleLogs {
 	rand.Seed(time.Now().UnixNano())
-	logAmount, _ := strconv.ParseInt(config.ConfigValues.App.LogAmount, 0, 0)
-	logAmountCast := int(logAmount)
-
-	for i := 0; i < logAmountCast; i++ {
+	for i := 0; i < config.LogAmountSet; i++ {
 		SampleLogEntry = GenerateLog()
 		fmt.Println(SampleLogEntry)
 		SampleLogsEntry.SampleLog = append(SampleLogsEntry.SampleLog, SampleLogEntry)
@@ -53,9 +49,9 @@ func GenerateMockLogs() models.SampleLogs {
 }
 
 func GenerateLog() (sampleLog models.SampleLog) {
-	sampleLog.Level = randomChoice(models.AvailableLevels)
-	sampleLog.Service = randomChoice(models.AvailableServices)
-	sampleLog.Message = selectMessage(sampleLog.Level)
+	sampleLog.Level = randomChoice(models.SampleLevels)
+	sampleLog.Service = randomChoice(models.SampleServices)
+	sampleLog.Message = selectSampleMessage(sampleLog.Level)
 	sampleLog.Time = randomTime()
 	return sampleLog
 }
