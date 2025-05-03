375d29a59955f2b2fdd47ba5e5f9354b5331e3e6
Hursow <thuanraellombardi@gmail.com>
2025-05-02 22:30:55 -0300

    refactor WriteLogsToFile function to WriteSampleLogsToFile for clarity and improve log saving process

--- internal/storage/file.go
+++ internal/storage/file.go
@@ -7,18 +7,16 @@ import (
 	models "tidybeaver/pkg/models"
 )
 
-func WriteLogsToFile(receivedLogs models.SampleLogs) {
-	fmt.Println("Logs received to be writen to file (text):")
-	fmt.Println(receivedLogs)
-	receivedLogsByte := fmt.Sprint(receivedLogs)
-	os.WriteFile(("TidyBeaverLogs.txt"), []byte(receivedLogsByte), 0644)
+func WriteSampleLogsToFile(sampleLogs models.SampleLogs) {
+	receivedLogsByte := fmt.Sprint(sampleLogs)
+	os.WriteFile((`.\Logs\TidyBeaverSampleLogs.txt`), []byte(receivedLogsByte), 0644)
+	fmt.Println("Sample Logs saved as Text")
 
-	encodedLogs, err := json.Marshal(receivedLogs)
+	encodedSampleLogs, err := json.Marshal(sampleLogs)
 	if err != nil {
 		return
+	} else {
+		os.WriteFile((`.\Logs\TidyBeaverSampleLogs.json`), encodedSampleLogs, 0644)
+		fmt.Println("Sample Logs saved as Json")
 	}
-
-	fmt.Println("Logs received to be writen to file (json):")
-	fmt.Println(string(encodedLogs))
-	os.WriteFile(("TidyBeaverLogs.json"), encodedLogs, 0644)
 }
