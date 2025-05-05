3a946bb61fcf04c1bf0481e7c7406ef095c18e9d
Hursow <thuanraellombardi@gmail.com>
2025-05-05 20:54:55 -0300

    refactor: simplify GetLogsFromOS function and update WindowsEventLog structure for improved clarity

--- internal/sources/os.go
+++ internal/sources/os.go
@@ -1,77 +1,52 @@
 package sources
 
 import (
-	"bufio"
-	"bytes"
+	"encoding/json"
 	"fmt"
-	"os"
 	"os/exec"
-	"regexp"
-	"strings"
 	config "tidybeaver/internal/config"
 )
 
 type WindowsEventLogs struct {
-	WindowsEventLogs []WindowsEventLogs
+	WindowsEventLogs []WindowsEventLog
 }
 
 type WindowsEventLog struct {
-	Index      string `json:"Index"`
-	Time       string `json:"Time"`
-	EntryType  string `json:"EntryType"`
-	Source     string `json:"Source"`
-	InstanceID string `json:"InstanceID"`
-	Message    string `json:"Message"`
+	MachineName        string   `json:"MachineName"`
+	Data               []string `json:"Data"`
+	Index              int      `json:"Index"`
+	Category           string   `json:"Category"`
+	CategoryNumber     int      `json:"CategoryNumber"`
+	EventID            int      `json:"EventID"`
+	EntryType          int      `json:"EntryType"`
+	Message            string   `json:"Message"`
+	Source             string   `json:"Source"`
+	ReplacementStrings []string `json:"ReplacementStrings"`
+	InstanceID         int      `json:"InstanceID"`
+	TimeGenerated      string   `json:"TimeGenerated"`
+	TimeWritten        string   `json:"TimeWritten"`
+	UserName           string   `json:"UserName"`
+	SplitLines         string   `json:"SplitLines"`
+	Container          string   `json:"Container"`
 }
 
-var windowsEventLog WindowsEventLog
+var windowsEventLogs WindowsEventLogs
 
-func GetLogsFromOS() {
-	cmd := exec.Command("powershell", "-Command", "Get-EventLog -LogName Application -Newest "+config.ConfigValues.App.LogAmount+" | Format-Table -AutoSize")
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
+func GetLogsFromOS() WindowsEventLogs {
+	cmd := exec.Command("powershell", "-Command", "Get-EventLog -LogName Application -Newest "+config.ConfigValues.App.LogAmount+" | ConvertTo-Json -Depth 2")
+	out, err := cmd.Output()
 	if err != nil {
 		fmt.Printf("Error running PowerShell command: %v\n", err)
-		return
-	}
-	fmt.Println("Application Event Logs: ", out.String())
-	os.WriteFile("TBEventLogs.txt", out.Bytes(), 0644)
-
-	stringz := out.String()
-	strLines := SplitLines(stringz)
-
-	for i := 0; i < len(strLines); i++ {
-
-		regex := regexp.MustCompile(`^.{1}(.{6})`)
-		test := regex.FindString(strLines[i])
-		fmt.Println("test: ", test)
-
-		fmt.Println("strLine: ", strings.Split(strLines[i], strLines[i]))
-		// windowsEventLog.Index =	string(regex.Split(strLines[i], -1))
-		// windowsEventLog.Time = regexp.MustCompile("^.{7}(.{19})")
-		// windowsEventLog.EntryType = regexp.MustCompile("^.{10}(.{5})")
-		// windowsEventLog.Source = regexp.MustCompile("^.{10}(.{5})")
-		// windowsEventLog.Message = regexp.MustCompile("^.{10}(.{5})")
+		return windowsEventLogs
 	}
 
-	// receivedLogs := ""
-	// encodedLogs, err := json.Marshal(receivedLogs)
-	// if err != nil {
-	// 	return
-	// }
+	fmt.Println("outputString: " + string(out))
 
-	// fmt.Println("Logs received to be writen to file (json):")
-	// fmt.Println(string(encodedLogs))
-	// os.WriteFile(("TBEventLogs.json"), encodedLogs, 0644)
-}
-
-func SplitLines(s string) []string {
-	var lines []string
-	sc := bufio.NewScanner(strings.NewReader(s))
-	for sc.Scan() {
-		lines = append(lines, sc.Text())
-
+	err = json.Unmarshal(out, &windowsEventLogs.WindowsEventLogs)
+	if err != nil {
+		fmt.Printf("Error unmarshaling JSON: %v\n", err)
+		return windowsEventLogs
 	}
-	return lines
+
+	return windowsEventLogs
 }
