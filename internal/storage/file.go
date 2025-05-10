87c21878efee10ae8c0ce15ccd11d0dc811d15ea
Hursow <thuanraellombardi@gmail.com>
2025-05-10 12:22:37 -0300

    fix: remove duplicate WriteLogsToFile function for clarity

--- internal/storage/file.go
+++ internal/storage/file.go
@@ -16,3 +16,13 @@ func WriteSampleLogsToFile(sampleLogs models.SampleLogs) {
 		fmt.Println("Sample Logs saved as Json")
 	}
 }
+
+func WriteLogsToFile(Logs interface{}) {
+	encodedLogs, err := json.Marshal(Logs)
+	if err != nil {
+		return
+	} else {
+		os.WriteFile((`.\Logs\TidyBeaverAdaptedLogs.json`), encodedLogs, 0644)
+		fmt.Println("Logs saved as Json")
+	}
+}
