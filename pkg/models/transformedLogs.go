b2e9bf4c4c37d2f5e7203d49468383a88c9917a5
Hursow <thuanraellombardi@gmail.com>
2025-05-12 17:07:56 -0300

    feat: add TransformedLogs and TransformedLog types for log handling

---
+++ pkg/models/transformedLogs.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"time"
+)
+
+type TransformedLogs struct {
+	TransformedLog []TransformedLog
+}
+
+type TransformedLog struct {
+	Level      string    `json:"Level"`
+	Service    string    `json:"Service"`
+	Message    string    `json:"Message"`
+	Time       time.Time `json:"Time"`
+	Index      int       `json:"Index"`
+	EntryType  string    `json:"EntryType"`
+	Source     string    `json:"Source"`
+	InstanceID int       `json:"InstanceID"`
+}
