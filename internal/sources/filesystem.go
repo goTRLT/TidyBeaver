417c02f2506ae006a12ebf5e5bd03ee83fcb7757
Hursow <thuanraellombardi@gmail.com>
2025-05-12 17:21:00 -0300

    fix: remove unnecessary blank lines in GetLogsFromFS function

--- internal/sources/filesystem.go
+++ internal/sources/filesystem.go
@@ -13,24 +13,30 @@ var FileDetailedLog models.TransformedLog
 
 func GetLogsFromFS() models.TransformedLogs {
 	files, err := os.ReadDir(`.\Logs`)
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Println("Files: ", files)
 
 	for _, file := range files {
 		logFile, err := os.Open(`.\Logs\` + file.Name())
+
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		defer logFile.Close()
 		decodedJson := json.NewDecoder(logFile)
 		decodedJson.Decode(&FileDetailedLogs)
 		indentedDetailedLog, err := json.MarshalIndent(FileDetailedLog, "", "  ")
+
 		if err != nil {
 			fmt.Println("Error marshalling the Indented Detailed Log:", err)
 			return FileDetailedLogs
 		}
+		
 		fmt.Println("Detailed Log: ", string(indentedDetailedLog))
 	}
 	return FileDetailedLogs
