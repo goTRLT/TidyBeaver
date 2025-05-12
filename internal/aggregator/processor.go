939b2ccccc432ed2be39da18cb07078f8ce25b4f
Hursow <thuanraellombardi@gmail.com>
2025-05-12 17:07:37 -0300

    fix: implement TransformLogs function to handle various log sources

--- internal/aggregator/processor.go
+++ internal/aggregator/processor.go
@@ -1,1 +1,25 @@
 package aggregator
+
+import (
+	config "tidybeaver/internal/config"
+	source "tidybeaver/internal/sources"
+)
+
+func TransformLogs() {
+	if config.UserInputConfigValues.UseSampleLogs {
+		return
+	} else {
+		if config.UserInputConfigValues.UseAPI {
+		}
+		if config.UserInputConfigValues.UseDatabase {
+		}
+		if config.UserInputConfigValues.UseFileSystem {
+			FSLogs = source.GetLogsFromFS()
+		}
+		if config.UserInputConfigValues.UseMicroservice {
+		}
+		if config.UserInputConfigValues.UseWindowsEvents {
+			OSLogs = source.GetLogsFromOS()
+		}
+	}
+}
