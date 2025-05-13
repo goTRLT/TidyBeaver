5c236e59a78b5167f9f815e924f0cffe29e22928
Hursow <thuanraellombardi@gmail.com>
2025-05-13 17:02:12 -0300

    fix: refactor GetLogsFromOS function to return error and improve error handling

--- internal/sources/os.go
+++ internal/sources/os.go
@@ -5,50 +5,25 @@ import (
 	"fmt"
 	"os/exec"
 	config "tidybeaver/internal/config"
+	models "tidybeaver/pkg/models"
 )
 
-type WindowsEventLogs struct {
-	WindowsEventLogs []WindowsEventLog
-}
-
-type WindowsEventLog struct {
-	MachineName        string   `json:"MachineName"`
-	Data               []string `json:"Data"`
-	Index              int      `json:"Index"`
-	Category           string   `json:"Category"`
-	CategoryNumber     int      `json:"CategoryNumber"`
-	EventID            int      `json:"EventID"`
-	EntryType          int      `json:"EntryType"`
-	Message            string   `json:"Message"`
-	Source             string   `json:"Source"`
-	ReplacementStrings []string `json:"ReplacementStrings"`
-	InstanceID         int      `json:"InstanceID"`
-	TimeGenerated      string   `json:"TimeGenerated"`
-	TimeWritten        string   `json:"TimeWritten"`
-	UserName           string   `json:"UserName"`
-	SplitLines         string   `json:"SplitLines"`
-	Container          string   `json:"Container"`
-}
-
-var windowsEventLogs WindowsEventLogs
-
-func GetLogsFromOS() WindowsEventLogs {
+func GetLogsFromOS() (windowsEventLogs models.WindowsEventLogs, err error) {
 	cmd := exec.Command("powershell", "-Command", "Get-EventLog -LogName Application -Newest "+config.ConfigValues.App.LogAmount+" | ConvertTo-Json -Depth 2")
 	out, err := cmd.Output()
 
 	if err != nil {
 		fmt.Printf("Error running PowerShell command: %v\n", err)
-		return windowsEventLogs
+		return windowsEventLogs, err
 	}
 
 	err = json.Unmarshal(out, &windowsEventLogs.WindowsEventLogs)
-	
+
 	if err != nil {
 		fmt.Printf("Error unmarshaling JSON: %v\n", err)
-		return windowsEventLogs
+		return windowsEventLogs, err
 	}
 
 	fmt.Print(windowsEventLogs)
-	return windowsEventLogs
-
+	return windowsEventLogs, err
 }
