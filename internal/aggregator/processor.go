5e760229a10709ab137c36d2f138b943cb0bdf49
Hursow <thuanraellombardi@gmail.com>
2025-05-16 15:51:05 -0300

    refactor: update log types in processor functions for consistency

--- internal/aggregator/processor.go
+++ internal/aggregator/processor.go
@@ -4,9 +4,9 @@ import (
 	"tidybeaver/pkg/models"
 )
 
-func TransformSampleLogs(MockLogs *models.SampleLogs)         {}
-func TransformAPILogs(APILogs *[]string)                      {}
-func TransformDBLogs(DBLogs *[]string)                        {}
-func TransformFSLogs(TransformedLogs *models.TransformedLogs) {}
-func TransformMSVLogs(MSVLogs *[]string)                      {}
-func TransformOSLogs(OSLogs *models.WindowsEventLogs)         {}
+func TransformSampleLogs(MockLogs *models.SampleLogs)      {}
+func TransformAPILogs(APILogs *[]string)                   {}
+func TransformDBLogs(DBLogs *[]string)                     {}
+func TransformFSLogs(TransformedLogs *models.StandardLogs) {}
+func TransformMSVLogs(MSVLogs *[]string)                   {}
+func TransformOSLogs(OSLogs *models.OSLogs)                {}
