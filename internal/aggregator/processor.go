a3de9000ff1d01caf2ff407a3f1692ed2b670735
Hursow <thuanraellombardi@gmail.com>
2025-05-17 17:16:59 -0300

    refactor: update TransformSampleLogs function to return AggregatedLog and streamline log processing

--- internal/aggregator/processor.go
+++ internal/aggregator/processor.go
@@ -6,26 +6,65 @@ import (
 	"time"
 )
 
-func TransformSampleLogs(SampleLogs *models.SampleLogs) models.AggregatedLogs {
-	var aggregatedLogs models.AggregatedLogs
-	var aggregatedLog []models.AggregatedLog
+func TransformSampleLogs(SampleLogs *models.SampleLogs) (aggregatedLogs models.AggregatedLog) {
+	var aggregatedLog models.AggregatedLog
 
 	for _, val := range SampleLogs.SampleLog {
-		for _, aval := range aggregatedLog {
-			for _, tt := range aval {
-				tt.Category = val.Level
-				tt.Message = val.Message
-				tt.Service = val.Service
-				tt.TimeGenerated = val.Time
-				tt.Source = "Sample Logs"
-				tt.Environment = "Dev"
-				tt.TimeWritten = time.Now()
-				append(aggregatedLog, tt)
-			}
+		for _, tt := range aggregatedLog {
+			tt.Category = val.Level
+			tt.Message = val.Message
+			tt.Service = val.Service
+			tt.TimeGenerated = val.Time
+			tt.Source = "Sample Logs"
+			tt.Environment = "Dev"
+			tt.TimeWritten = time.Now()
+			tt.CategoryNumber = 0
+			tt.Checksum = ""
+			tt.ClientIP = ""
+			tt.Column = ""
+			tt.Component = ""
+			tt.ComputerName = ""
+			tt.Constraint = ""
+			tt.Container = ""
+			tt.CorrelationID = ""
+			tt.Data = []string{"", ""}
+			tt.Datatype = ""
+			tt.Detail = ""
+			tt.Endpoint = ""
+			tt.EntryType = ""
+			tt.Errcode = ""
+			tt.EventID = ""
+			tt.EventType = ""
+			tt.FilePath = ""
+			tt.FileSize = 0
+			tt.Host = ""
+			tt.HTTPMethod = ""
+			tt.Index = 0
+			tt.InstanceID = 0
+			tt.LatencyMs = 0
+			tt.Level = ""
+			tt.LineNumber = 0
+			tt.LogName = ""
+			tt.MachineName = ""
+			tt.RequestBody = ""
+			tt.ReplacementStrings = []string{"", ""}
+			tt.ResponseBody = ""
+			tt.RowsAffected = 0
+			tt.Schema = ""
+			tt.SplitLines = ""
+			tt.SpanID = ""
+			tt.StatusCode = 0
+			tt.TableName = ""
+			tt.TransactionID = ""
+			tt.UserAgent = ""
+			tt.UserID = ""
+			tt.UserName = ""
+			tt.Query = ""
+			append(aggregatedLogs, tt)
 		}
 	}
 	fmt.Println(aggregatedLog)
-	return aggregatedLogs
+	return aggregatedLog
 }
 
 // func TransformAPILogs(APILogs *[]string) models.AggregatedLogs    {}
