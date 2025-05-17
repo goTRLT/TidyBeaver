d28046cdaf2932f6523166c8892b48c055d13ddb
Hursow <thuanraellombardi@gmail.com>
2025-05-17 16:48:51 -0300

    feat: implement TransformSampleLogs function for processing sample log data

--- internal/aggregator/processor.go
+++ internal/aggregator/processor.go
@@ -1,8 +1,32 @@
 package aggregator
 
-// import (
-// 	"tidybeaver/pkg/models"
-// )
+import (
+	"fmt"
+	"tidybeaver/pkg/models"
+	"time"
+)
+
+func TransformSampleLogs(SampleLogs *models.SampleLogs) models.AggregatedLogs {
+	var aggregatedLogs models.AggregatedLogs
+	var aggregatedLog []models.AggregatedLog
+
+	for _, val := range SampleLogs.SampleLog {
+		for _, aval := range aggregatedLog {
+			for _, tt := range aval {
+				tt.Category = val.Level
+				tt.Message = val.Message
+				tt.Service = val.Service
+				tt.TimeGenerated = val.Time
+				tt.Source = "Sample Logs"
+				tt.Environment = "Dev"
+				tt.TimeWritten = time.Now()
+				append(aggregatedLog, tt)
+			}
+		}
+	}
+	fmt.Println(aggregatedLog)
+	return aggregatedLogs
+}
 
 // func TransformAPILogs(APILogs *[]string) models.AggregatedLogs    {}
 // func TransformDBLogs(DBLogs *models.DBLogs) models.AggregatedLogs {}
