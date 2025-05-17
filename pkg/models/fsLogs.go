cdd66e70d006aeb7ab232d843625e1b4083461f8
Hursow <thuanraellombardi@gmail.com>
2025-05-17 15:59:48 -0300

    feat: add FSLogs type for structured log data representation

---
+++ pkg/models/fsLogs.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+)
+
+type FSLogs struct {
+	FSLog []struct {
+		Level      string    `json:"Level"`
+		Service    string    `json:"Service"`
+		Message    string    `json:"Message"`
+		Time       time.Time `json:"Time"`
+		Index      int       `json:"Index"`
+		EntryType  string    `json:"EntryType"`
+		Source     string    `json:"Source"`
+		InstanceID int       `json:"InstanceID"`
+	}
+}
