66c96c9321dde39fea980e70b20022f8534930ea
Hursow <thuanraellombardi@gmail.com>
2025-05-19 17:49:45 -0300

    refactor: change EntryType field from string to int type in FSLogs struct

--- pkg/models/fsLogs.go
+++ pkg/models/fsLogs.go
@@ -11,7 +11,7 @@ type FSLogs struct {
 		Message    string    `json:"Message"`
 		Time       time.Time `json:"Time"`
 		Index      int       `json:"Index"`
-		EntryType  string    `json:"EntryType"`
+		EntryType  int       `json:"EntryType"`
 		Source     string    `json:"Source"`
 		InstanceID int       `json:"InstanceID"`
 	}
