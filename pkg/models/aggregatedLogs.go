73b7f8787764926411933834a634a64ea4c87bfd
Hursow <thuanraellombardi@gmail.com>
2025-05-19 15:48:23 -0300

    refactor: restructure AggregatedLogs type and add New function for log creation

--- pkg/models/aggregatedLogs.go
+++ pkg/models/aggregatedLogs.go
@@ -2,7 +2,11 @@ package models
 
 import "time"
 
-type AggregatedLog []struct {
+type AggregatedLogs struct {
+	AggregatedLogSlice []AggregatedLog
+}
+
+type AggregatedLog struct {
 	Category           string    `json:"category,omitempty"`
 	CategoryNumber     int       `json:"category_number,omitempty"`
 	Checksum           string    `json:"checksum,omitempty"`
@@ -52,4 +56,108 @@ type AggregatedLog []struct {
 	UserID             string    `json:"user_id,omitempty"`
 	UserName           string    `json:"user_name,omitempty"`
 	Query              string    `json:"query,omitempty"`
+}
+
+func New(
+	Category string,
+	CategoryNumber int,
+	Checksum string,
+	ClientIP string,
+	Column string,
+	Component string,
+	ComputerName string,
+	Constraint string,
+	Container string,
+	CorrelationID string,
+	Data []string,
+	Datatype string,
+	Detail string,
+	Endpoint string,
+	EntryType string,
+	Environment string,
+	Errcode string,
+	EventID string,
+	EventType string,
+	FilePath string,
+	FileSize int64,
+	Host string,
+	HTTPMethod string,
+	Index int,
+	InstanceID int,
+	LatencyMs int,
+	Level string,
+	LineNumber int,
+	LogName string,
+	MachineName string,
+	Message string,
+	RequestBody string,
+	ReplacementStrings []string,
+	ResponseBody string,
+	RowsAffected int,
+	Schema string,
+	Service string,
+	Source string,
+	SplitLines string,
+	SpanID string,
+	StatusCode int,
+	TableName string,
+	TimeGenerated time.Time,
+	TimeWritten time.Time,
+	TransactionID string,
+	UserAgent string,
+	UserID string,
+	UserName string,
+	Query string,
+) AggregatedLog {
+	return AggregatedLog{
+		Category:           Category,
+		CategoryNumber:     CategoryNumber,
+		Checksum:           Checksum,
+		ClientIP:           ClientIP,
+		Column:             Column,
+		Component:          Component,
+		ComputerName:       ComputerName,
+		Constraint:         Constraint,
+		Container:          Container,
+		CorrelationID:      CorrelationID,
+		Data:               Data,
+		Datatype:           Datatype,
+		Detail:             Detail,
+		Endpoint:           Endpoint,
+		EntryType:          EntryType,
+		Environment:        Environment,
+		Errcode:            Errcode,
+		EventID:            EventID,
+		EventType:          EventType,
+		FilePath:           FilePath,
+		FileSize:           FileSize,
+		Host:               Host,
+		HTTPMethod:         HTTPMethod,
+		Index:              Index,
+		InstanceID:         InstanceID,
+		LatencyMs:          LatencyMs,
+		Level:              Level,
+		LineNumber:         LineNumber,
+		LogName:            LogName,
+		MachineName:        MachineName,
+		Message:            Message,
+		RequestBody:        RequestBody,
+		ReplacementStrings: ReplacementStrings,
+		ResponseBody:       ResponseBody,
+		RowsAffected:       RowsAffected,
+		Schema:             Schema,
+		Service:            Service,
+		Source:             Source,
+		SplitLines:         SplitLines,
+		SpanID:             SpanID,
+		StatusCode:         StatusCode,
+		TableName:          TableName,
+		TimeGenerated:      TimeGenerated,
+		TimeWritten:        TimeWritten,
+		TransactionID:      TransactionID,
+		UserAgent:          UserAgent,
+		UserID:             UserID,
+		UserName:           UserName,
+		Query:              Query,
+	}
 }
