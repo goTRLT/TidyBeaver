add5776ceac949816e7ed937aa8567305c52f5ae
Hursow <thuanraellombardi@gmail.com>
2025-05-19 15:51:49 -0300

    feat: implement TransformFSLogs function for processing FSLogs

--- internal/aggregator/processor.go
+++ internal/aggregator/processor.go
@@ -67,6 +67,67 @@ func TransformSampleLogs(SampleLogs *models.SampleLogs) (aggregatedLogs []models
 	return transformedLogs
 }
 
+func TransformFSLogs(FSLogs *models.FSLogs) (aggregatedLogs []models.AggregatedLog) {
+
+	var transformedLogs []models.AggregatedLog
+	for _, val := range FSLogs.FSLog {
+		test := models.New(
+			"",
+			0,
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			[]string{"", ""},
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			0,
+			"",
+			"",
+			0,
+			0,
+			0,
+			val.Level,
+			0,
+			"",
+			"",
+			val.Message,
+			"",
+			[]string{"", ""},
+			"",
+			0,
+			"",
+			val.Service,
+			"Sample Log",
+			"",
+			"",
+			0,
+			"",
+			val.Time,
+			time.Now(),
+			"",
+			"",
+			"",
+			"",
+			"",
+		)
+		transformedLogs = append(transformedLogs, test)
+	}
+	fmt.Println(transformedLogs)
+	return transformedLogs
+}
+
 // func TransformAPILogs(APILogs *[]string) models.AggregatedLogs    {}
 // func TransformDBLogs(DBLogs *models.DBLogs) models.AggregatedLogs {}
 // func TransformFSLogs(FSLogs *models.FSLogs) models.AggregatedLogs {}
