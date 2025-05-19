9767c49047830f46c2c8e749f3b696b11ba32f1c
Hursow <thuanraellombardi@gmail.com>
2025-05-19 15:48:03 -0300

    refactor: update TransformSampleLogs function to improve log aggregation and streamline data processing

--- internal/aggregator/processor.go
+++ internal/aggregator/processor.go
@@ -6,65 +6,65 @@ import (
 	"time"
 )
 
-func TransformSampleLogs(SampleLogs *models.SampleLogs) (aggregatedLogs models.AggregatedLog) {
-	var aggregatedLog models.AggregatedLog
+func TransformSampleLogs(SampleLogs *models.SampleLogs) (aggregatedLogs []models.AggregatedLog) {
 
+	var aggregatedLogs2 []models.AggregatedLog
 	for _, val := range SampleLogs.SampleLog {
-		for _, tt := range aggregatedLog {
-			tt.Category = val.Level
-			tt.Message = val.Message
-			tt.Service = val.Service
-			tt.TimeGenerated = val.Time
-			tt.Source = "Sample Logs"
-			tt.Environment = "Dev"
-			tt.TimeWritten = time.Now()
-			tt.CategoryNumber = 0
-			tt.Checksum = ""
-			tt.ClientIP = ""
-			tt.Column = ""
-			tt.Component = ""
-			tt.ComputerName = ""
-			tt.Constraint = ""
-			tt.Container = ""
-			tt.CorrelationID = ""
-			tt.Data = []string{"", ""}
-			tt.Datatype = ""
-			tt.Detail = ""
-			tt.Endpoint = ""
-			tt.EntryType = ""
-			tt.Errcode = ""
-			tt.EventID = ""
-			tt.EventType = ""
-			tt.FilePath = ""
-			tt.FileSize = 0
-			tt.Host = ""
-			tt.HTTPMethod = ""
-			tt.Index = 0
-			tt.InstanceID = 0
-			tt.LatencyMs = 0
-			tt.Level = ""
-			tt.LineNumber = 0
-			tt.LogName = ""
-			tt.MachineName = ""
-			tt.RequestBody = ""
-			tt.ReplacementStrings = []string{"", ""}
-			tt.ResponseBody = ""
-			tt.RowsAffected = 0
-			tt.Schema = ""
-			tt.SplitLines = ""
-			tt.SpanID = ""
-			tt.StatusCode = 0
-			tt.TableName = ""
-			tt.TransactionID = ""
-			tt.UserAgent = ""
-			tt.UserID = ""
-			tt.UserName = ""
-			tt.Query = ""
-			append(aggregatedLogs, tt)
-		}
+		test := models.New(
+			"",
+			0,
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			[]string{"", ""},
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			0,
+			"",
+			"",
+			0,
+			0,
+			0,
+			val.Level,
+			0,
+			"",
+			"",
+			val.Message,
+			"",
+			[]string{"", ""},
+			"",
+			0,
+			"",
+			val.Service,
+			"Sample Log",
+			"",
+			"",
+			0,
+			"",
+			val.Time,
+			time.Now(),
+			"",
+			"",
+			"",
+			"",
+			"",
+		)
+		aggregatedLogs2 = append(aggregatedLogs2, test)
 	}
-	fmt.Println(aggregatedLog)
-	return aggregatedLog
+	fmt.Println(aggregatedLogs2)
+	return aggregatedLogs2
 }
 
 // func TransformAPILogs(APILogs *[]string) models.AggregatedLogs    {}
