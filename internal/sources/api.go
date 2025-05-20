4fdd0974bbe30e4441145436dd86acd2b15ccc9f
Hursow <thuanraellombardi@gmail.com>
2025-05-20 17:57:15 -0300

    refactor: fix FetchAPILogs to correctly append API log responses

--- internal/sources/api.go
+++ internal/sources/api.go
@@ -1,1 +1,34 @@
 package sources
+
+import (
+	"encoding/json"
+	"net/http"
+	"tidybeaver/internal/config"
+	"tidybeaver/pkg/models"
+)
+
+func FetchAPILogs() (APILogs []models.APILogs, err error) {
+
+	//TODO Add Timeout
+	resp, err := http.Get(config.ConfigValues.API.BaseURL + config.ConfigValues.App.LogAmount)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	// fmt.Println("Resp ", resp)
+
+	var responses []models.APILog
+
+	if err := json.NewDecoder(resp.Body).Decode(&responses); err != nil {
+		return nil, err
+	}
+
+	// fmt.Println("ActResp ", responses)
+
+	append(APILogs, responses...)
+
+	return APILogs, err
+}
