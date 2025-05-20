4dd88eff785abef46734648d0ed023f666f5c294
Hursow <thuanraellombardi@gmail.com>
2025-05-20 13:44:33 -0300

    refactor: remove unused fmt import and replace fmt.Println with log.Fatal for error handling in FetchOSLogs and RunCommands functions

--- internal/sources/os.go
+++ internal/sources/os.go
@@ -2,10 +2,8 @@ package sources
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"os/exec"
-	config "tidybeaver/internal/config"
 	models "tidybeaver/pkg/models"
 )
 
@@ -17,7 +15,7 @@ func FetchOSLogs() (OSLogs models.OSLogs, err error) {
 	}
 
 	out, err := MergeJSONOutputs(output1, output2, output3)
-	fmt.Println("out", string(out))
+	// fmt.Println("out", string(out))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,37 +26,38 @@ func FetchOSLogs() (OSLogs models.OSLogs, err error) {
 		log.Fatal(err)
 	}
 
-	fmt.Print("wel ", OSLogs)
+	// fmt.Print("wel ", OSLogs)
 
 	return OSLogs, err
 }
 
 func RunCommands() (outputApp []byte, outputSys []byte, outputSec []byte, err error) {
-	cmdApp := exec.Command("powershell", "-Command", "Get-EventLog -LogName Application -Newest "+config.ConfigValues.App.LogAmount+" | ConvertTo-Json -Depth 2; ")
+	// test := config.ConfigValues.App.LogAmount
+	cmdApp := exec.Command("powershell", "-Command", "Get-EventLog -LogName Application -Newest 20 | ConvertTo-Json -Depth 2; ")
 	outputApp, errApp := cmdApp.Output()
 
 	if errApp != nil {
-		fmt.Printf("Error running PowerShell command: %v\n", errApp)
+		log.Fatal("Error running PowerShell command: %v\n", errApp)
 		return outputApp, outputSys, outputSec, errApp
 	}
 
 	//fmt.Print("outputApp", string(outputApp))
 
-	cmdSys := exec.Command("powershell", "-Command", "Get-EventLog -LogName System -Newest "+config.ConfigValues.App.LogAmount+" | ConvertTo-Json -Depth 2; ")
+	cmdSys := exec.Command("powershell", "-Command", "Get-EventLog -LogName System -Newest 20 | ConvertTo-Json -Depth 2; ")
 	outputSys, errSys := cmdSys.Output()
 
 	if errSys != nil {
-		fmt.Printf("Error running PowerShell command: %v\n", errSys)
+		log.Fatal("Error running PowerShell command: %v\n", errSys)
 		return outputApp, outputSys, outputSec, errSys
 	}
 
 	//fmt.Print("outputSys", string(outputSys))
 
-	cmdSec := exec.Command("powershell", "-Command", "Get-EventLog -LogName Security -Newest "+config.ConfigValues.App.LogAmount+" | ConvertTo-Json -Depth 2")
+	cmdSec := exec.Command("powershell", "-Command", "Get-EventLog -LogName Security -Newest 20 | ConvertTo-Json -Depth 2")
 	outputSec, errSec := cmdSec.Output()
 
 	if errSec != nil {
-		fmt.Printf("Error running PowerShell command: %v\n", errSec)
+		log.Fatal("Error running PowerShell command: %v\n", errSec)
 		return outputApp, outputSys, outputSec, errSec
 	}
 
@@ -74,7 +73,7 @@ func MergeJSONOutputs(outputs ...[]byte) ([]byte, error) {
 		err := json.Unmarshal(vals, &temp)
 
 		if err != nil {
-			fmt.Printf("Error unmarshaling JSON: %v\n", err)
+			log.Fatal("Error unmarshaling JSON: %v\n", err)
 			return vals, err
 		}
 		for _, v := range temp {
