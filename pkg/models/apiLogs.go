78999e8ce3c17630242c0b9fd6159d092e54cf0a
Hursow <thuanraellombardi@gmail.com>
2025-05-21 11:27:32 -0300

    refactor: update Timestamp field in APILog struct to use time.Time type

--- pkg/models/apiLogs.go
+++ pkg/models/apiLogs.go
@@ -1,14 +1,16 @@
 package models
 
+import "time"
+
 type APILogs struct {
 	APILog []APILog
 }
 
 type APILog struct {
-	StatusCode int    `json:"status_code"`
-	Status     string `json:"status"`
-	Message    string `json:"message"`
-	Timestamp  string `json:"timestamp"`
-	Path       string `json:"path"`
-	RequestID  string `json:"request_id"`
+	StatusCode int       `json:"status_code"`
+	Status     string    `json:"status"`
+	Message    string    `json:"message"`
+	Timestamp  time.Time `json:"timestamp"`
+	Path       string    `json:"path"`
+	RequestID  string    `json:"request_id"`
 }
