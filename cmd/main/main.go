c3fea9c5a991a1cd5497622a810bfc0ede61d685
Hursow <thuanraellombardi@gmail.com>
2025-05-24 13:15:48 -0300

    refactor: add environment variable logging in main function

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	aggregator "tidybeaver/internal/aggregator"
 	config "tidybeaver/internal/config"
 )
@@ -10,6 +11,13 @@ var Configurations config.Configs
 var UserInputConfigurations config.UserInputConfigurations
 
 func main() {
+	fmt.Println(os.Getenv("DB_HOST"))
+	fmt.Println(os.Getenv("DB_PORT"))
+	fmt.Println(os.Getenv("DB_USER"))
+	fmt.Println(os.Getenv("DB_PW"))
+	fmt.Println(os.Getenv("DB_NAME"))
+	fmt.Println(os.Getenv("SSLMODE"))
+
 	fmt.Println("The Tidy Beaver wakes up and says: Hello there!")
 	config.Init()
 	aggregator.Init()
