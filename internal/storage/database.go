102e5b0fc057227ce9b428e9b7dfca2c604ce780
Hursow <thuanraellombardi@gmail.com>
2025-05-24 13:15:31 -0300

    refactor: update DBInsertLogs to use environment variables for database connection string

--- internal/storage/database.go
+++ internal/storage/database.go
@@ -4,15 +4,15 @@ import (
 	"database/sql"
 	"log"
 	"math/rand"
-	config "tidybeaver/internal/config"
+	"os"
 	models "tidybeaver/pkg/models"
 
 	"github.com/lib/pq"
-	_ "github.com/lib/pq"
 )
 
 func DBInsertLogs(logs *models.AggregatedLogs) {
-	connStr := `host=` + config.ConfigValues.Database.Host + ` port=` + config.ConfigValues.Database.Port + ` user=` + config.ConfigValues.Database.User + ` password=` + config.ConfigValues.Database.Password + ` dbname=` + config.ConfigValues.Database.Name + ` sslmode=` + config.ConfigValues.Database.SSLMode
+	// connStr := `host=` + config.ConfigValues.Database.Host + ` port=` + config.ConfigValues.Database.Port + ` user=` + config.ConfigValues.Database.User + ` password=` + config.ConfigValues.Database.Password + ` dbname=` + config.ConfigValues.Database.Name + ` sslmode=` + config.ConfigValues.Database.SSLMode
+	connStr := `host=` + os.Getenv("DB_HOST") + ` port=` + os.Getenv("DB_PORT") + ` user=` + os.Getenv("DB_USER") + ` password=` + os.Getenv("DB_PW") + ` dbname=` + os.Getenv("DB_NAME") + ` sslmode=` + os.Getenv("SSLMODE")
 	db, err := sql.Open("postgres", connStr)
 
 	if err != nil {
