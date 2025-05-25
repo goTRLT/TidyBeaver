1c2214a2691b3b73cee91cc0cdb6fab2a3bc2f69
Hursow <thuanraellombardi@gmail.com>
2025-05-25 15:12:31 -0300

    feat: add APILogs and APILog types for API logging

---
+++ pkg/types/apiLogs.go
@@ -0,0 +1,16 @@
+package types
+
+import "time"
+
+type APILogs struct {
+	APILog []APILog
+}
+
+type APILog struct {
+	StatusCode int       `json:"status_code"`
+	Status     string    `json:"status"`
+	Message    string    `json:"message"`
+	Timestamp  time.Time `json:"timestamp"`
+	Path       string    `json:"path"`
+	RequestID  string    `json:"request_id"`
+}
