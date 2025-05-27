c6d7334f971b50fb4e19aba88106b31ebcea864f
Hursow <thuanraellombardi@gmail.com>
2025-05-27 14:35:26 -0300

    feat: add DBLogs and DBLog types to models package

---
+++ pkg/models/dbLogs.go
@@ -0,0 +1,16 @@
+package models
+
+type DBLogs struct {
+	DBLog []DBLog
+}
+
+type DBLog struct {
+	Level      string `json:"level"`
+	Column     string `json:"column"`
+	Constraint string `json:"constraint"`
+	Datatype   string `json:"datatype"`
+	Table_name string `json:"table_name"`
+	Schema     string `json:"schema"`
+	Errcode    string `json:"errcode"`
+	Detail     string `json:"detail"`
+}
