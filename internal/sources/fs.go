869e0dac04a78fcb8327e898241a1d0612553f7b
Hursow <thuanraellombardi@gmail.com>
2025-05-27 14:34:43 -0300

    feat: implement FetchFSLogs function for reading filesystem logs

---
+++ internal/sources/fs.go
@@ -0,0 +1,53 @@
+package sources
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	models "tidybeaver/pkg/models"
+)
+
+func FetchFSLogs() (types models.FSLogs, err error) {
+	var FSLogs models.FSLogs
+	files, err := os.ReadDir(`.\logs`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Files: ", files)
+
+	for _, file := range files {
+		logFile, err := os.Open(`.logs\` + file.Name())
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer logFile.Close()
+		var tempLogs models.FSLogs
+
+		decodedJson := json.NewDecoder(logFile)
+		decodedJson.Decode(&tempLogs)
+
+		if err != nil {
+			log.Printf("Error decoding file %s: %v", file.Name(), err)
+			continue
+		}
+
+		FSLogs.FSLog = append(FSLogs.FSLog, tempLogs.FSLog...)
+
+		// indentedFSLog, err := json.MarshalIndent(tempLogs, "", "  ")
+
+		// if err != nil {
+		// 	log.Fatal("Error marshalling the Indented Detailed Log:", err)
+		// 	return FSLogs, err
+		// }
+
+		// fmt.Println("logFile: ", logFile.Name())
+		// fmt.Println("indentedFSLog: ", string(indentedFSLog))
+		// fmt.Println("FSLogs: ", FSLogs)
+	}
+	return FSLogs, err
+}
