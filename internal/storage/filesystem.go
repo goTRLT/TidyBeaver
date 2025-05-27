84ae416bc24eb0c118ac21d900fb344b82ea53cd
Hursow <thuanraellombardi@gmail.com>
2025-05-27 14:35:11 -0300

    feat: update JSONSaveLogs to use models package instead of types

--- internal/storage/filesystem.go
+++ internal/storage/filesystem.go
@@ -4,15 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	types "tidybeaver/pkg/types"
+	"tidybeaver/pkg/models"
 )
 
-func JSONSaveLogs(Logs *types.AggregatedLogs) {
+func JSONSaveLogs(Logs *models.AggregatedLogs) {
 	encodedLogs, err := json.Marshal(Logs)
 	if err != nil {
 		return
 	} else {
-		os.WriteFile((`.\Logs\TidyBeaverAdaptedLogs.json`), encodedLogs, 0644)
+		os.WriteFile((`.logs\TidyBeaverAdaptedLogs.json`), encodedLogs, 0644)
 		fmt.Println("Logs saved as Json")
 	}
 }
