e822d18d104fad0805938df9a12b4bbca45f4b13
Hursow <thuanraellombardi@gmail.com>
2025-05-28 12:43:43 -0300

    refactor: clean up package declaration and import statement in api.go

--- cmd/api.go
+++ cmd/api.go
@@ -1,14 +1,13 @@
-package cmd
+package main
 
 import (
 	"fmt"
 	"net/http"
-	"tidybeaver/internal/api"
+	api "tidybeaver/internal/api"
 )
 
 func InitAPI() {
 	http.HandleFunc("/api/random-response", api.ResponseHandler)
-
 	port := ":9090"
 	fmt.Println("Server running at http://localhost" + port)
 	http.ListenAndServe(port, nil)
