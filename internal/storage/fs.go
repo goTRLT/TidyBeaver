04a0ff264295f81614fe4a0297648a132f0f589a
Hursow <thuanraellombardi@gmail.com>
2025-05-28 12:46:01 -0300

    feat: add JSONSaveLogs function to save aggregated logs as JSON

---
+++ internal/storage/fs.go
@@ -0,0 +1,18 @@
+package storage
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	models "tidybeaver/pkg/models"
+)
+
+func JSONSaveLogs(Logs *models.AggregatedLogs) {
+	encodedLogs, err := json.Marshal(Logs)
+	if err != nil {
+		return
+	} else {
+		os.WriteFile((`.\logs\TidyBeaverAdaptedLogs.json`), encodedLogs, 0644)
+		fmt.Println("Logs saved as Json")
+	}
+}
