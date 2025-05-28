0f906714e2551c759ad2c92a50389ee62f6c8ba0
Hursow <thuanraellombardi@gmail.com>
2025-05-28 12:45:27 -0300

    refactor: clean up import statements and remove commented-out code in api.go

--- internal/sources/api.go
+++ internal/sources/api.go
@@ -4,13 +4,14 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"tidybeaver/internal/config"
-	"tidybeaver/pkg/models"
+	config "tidybeaver/internal/config"
+	models "tidybeaver/pkg/models"
 )
 
 func FetchAPILogs() (APILogs models.APILogs, err error) {
 
 	var APILogEntry []models.APILog
+	var responses []models.APILog
 	//TODO Add Timeout
 	resp, err := http.Get(config.ConfigValues.API.BaseURL + config.ConfigValues.App.LogAmount)
 
@@ -20,15 +21,9 @@ func FetchAPILogs() (APILogs models.APILogs, err error) {
 
 	defer resp.Body.Close()
 
-	// fmt.Println("Resp ", resp)
-
-	var responses []models.APILog
-
 	if err := json.NewDecoder(resp.Body).Decode(&responses); err != nil {
 		log.Fatal(err)
 	}
-
-	// fmt.Println("ActResp ", responses)
 
 	APILogs.APILog = append(APILogEntry, responses...)
 
