6d0001ac56eebb7b48bfb4f0f6f80241859d1cac
Hursow <thuanraellombardi@gmail.com>
2025-05-29 10:36:20 -0300

    refactor: replace LogEntry with MSVCLog struct for improved log handling

--- pkg/models/msvcLogs.go
+++ pkg/models/msvcLogs.go
@@ -1,11 +1,17 @@
 package models
 
-type LogEntry struct {
-	Timestamp     string `json:"timestamp"`
-	Service       string `json:"service"`
-	Level         string `json:"level"`
-	Message       string `json:"message"`
-	CorrelationID string `json:"correlation_id"`
-	RequestID     string `json:"request_id"`
-	Host          string `json:"host"`
+import "time"
+
+type MSVCLogs struct {
+	MSVCLog []MSVCLog
+}
+
+type MSVCLog struct {
+	Timestamp     time.Time `json:"timestamp"`
+	Service       string    `json:"service"`
+	Level         string    `json:"level"`
+	Message       string    `json:"message"`
+	CorrelationID string    `json:"correlation_id"`
+	RequestID     string    `json:"request_id"`
+	Host          string    `json:"host"`
 }
