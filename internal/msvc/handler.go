3b4351a61a23a46ee9046da4b6d744082aeaf663
Hursow <thuanraellombardi@gmail.com>
2025-05-29 11:08:20 -0300

    feat: implement dynamic log count based on 'amount' query parameter in MsvcLogHandler

--- internal/msvc/handler.go
+++ internal/msvc/handler.go
@@ -3,13 +3,27 @@ package msvc
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	models "tidybeaver/pkg/models"
 )
 
 func MsvcLogHandler(serviceName, hostname string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		entry := CreateRandomResponse(serviceName, hostname)
+		var result []models.MSVCLog
+		countStr := r.URL.Query().Get("amount")
+		amount := 1
+		if countStr != "" {
+			if parsed, err := strconv.Atoi(countStr); err == nil && parsed > 0 && parsed <= 100 {
+				amount = parsed
+			}
+		}
+
+		for i := 0; i < amount; i++ {
+			result = append(result, CreateRandomResponse(serviceName, hostname))
+		}
 
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(entry)
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(result)
 	}
 }
