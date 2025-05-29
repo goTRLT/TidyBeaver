9e6f2e8de6667b16cdabf8f7780ce4ed0e435ab0
Hursow <thuanraellombardi@gmail.com>
2025-05-29 09:18:57 -0300

    feat: implement MsvcLogHandler to handle logging for the service

---
+++ internal/msvc/handler.go
@@ -0,0 +1,15 @@
+package msvc
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func MsvcLogHandler(serviceName, hostname string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		entry := randomLogEntry(serviceName, hostname)
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(entry)
+	}
+}
