7c1d5f13a3852027af6f42d370dcf41872d7dc8d
Hursow <thuanraellombardi@gmail.com>
2025-05-29 10:35:52 -0300

    feat: enhance main function with additional logging and improved structure

--- cmd/main.go
+++ cmd/main.go
@@ -11,9 +11,32 @@ var Configurations config.Configs
 var UserInputConfigurations config.UserInputConfigurations
 
 func main() {
+	fmt.Println("Welcome to the TidyBeaver Log aggregator!")
+
+	time.Sleep(500 * time.Millisecond)
+
+	fmt.Println("TidyBeaver's API is now being built")
 	go InitAPI()
-	fmt.Println("The Tidy Beaver wakes up and says: Hello there!")
-	time.Sleep(1 * time.Second)
+	fmt.Println("Complete!")
+
+	time.Sleep(500 * time.Millisecond)
+
+	fmt.Println("TidyBeaver's Microservice now is being built")
+	go InitMSVC()
+	fmt.Println("Complete!")
+
+	time.Sleep(500 * time.Millisecond)
+
+	fmt.Println("TidyBeaver's configurations are now being set up")
 	config.Init()
-	aggregator.Init()
+	fmt.Println("Complete!")
+
+	time.Sleep(500 * time.Millisecond)
+
+	fmt.Println("TidyBeaver's Log Aggregator starts working")
+	for 1 != 2 {
+		aggregator.Init()
+		fmt.Println("TidyBeaver will rest for a minute before resuming it's work")
+		time.Sleep(1 * time.Minute)
+	}
 }
