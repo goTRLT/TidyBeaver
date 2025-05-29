4e2aac7aff84f8adefefea161f2f96b42d13ca2e
Hursow <thuanraellombardi@gmail.com>
2025-05-29 10:36:10 -0300

    fix: update log entry creation function in MsvcLogHandler

--- internal/msvc/handler.go
+++ internal/msvc/handler.go
@@ -7,7 +7,7 @@ import (
 
 func MsvcLogHandler(serviceName, hostname string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		entry := randomLogEntry(serviceName, hostname)
+		entry := CreateRandomResponse(serviceName, hostname)
 
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(entry)
