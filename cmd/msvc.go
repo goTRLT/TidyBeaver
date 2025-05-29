649a7f53c2724116459698d904b2e88b6db0abf9
Hursow <thuanraellombardi@gmail.com>
2025-05-29 09:18:48 -0300

    refactor: restructure InitMSVC function for improved readability and organization

--- cmd/msvc.go
+++ cmd/msvc.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	msvc "tidybeaver/internal/msvc"
+	"time"
+)
+
+func InitMSVC() {
+	rand.Seed(time.Now().UnixNano())
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "unknown-host"
+	}
+
+	serviceName := "log-generator-service"
+
+	http.HandleFunc("/log", msvc.MsvcLogHandler(serviceName, hostname))
+
+	log.Println("Starting log generator service on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
