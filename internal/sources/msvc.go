eaf5c187a965643e6ed763289e739e40aa601a03
Hursow <thuanraellombardi@gmail.com>
2025-05-29 11:08:23 -0300

    feat: implement GetMSVCLogs function to fetch and decode MSVC logs

--- internal/sources/msvc.go
+++ internal/sources/msvc.go
@@ -1,11 +1,43 @@
 package sources
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+	config "tidybeaver/internal/config"
 	models "tidybeaver/pkg/models"
+	"time"
 )
 
-func GetMSVCLogs() (m models.MSVCLogs, err error) {
-	fmt.Println("msvcLogs")
-	return m, err
+func GetMSVCLogs() (MSVCLogs models.MSVCLogs, err error) {
+	var MSVCLogEntry []models.MSVCLog
+	var responses []models.MSVCLog
+
+	timeoutSecondsStr := config.EnvVar["MSVC_TIMEOUTSECONDS"]
+	timeoutSeconds, err := strconv.Atoi(timeoutSecondsStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := &http.Client{
+		Timeout: -time.Duration(timeoutSeconds) * time.Second,
+	}
+
+	resp, err := client.Get(config.EnvVar["MSVC_BASEURL"] + config.ConfigValues.App.LogAmount)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	if err := json.NewDecoder(resp.Body).Decode(&responses); err != nil {
+		log.Fatal(err)
+	}
+
+	MSVCLogs.MSVCLog = append(MSVCLogEntry, responses...)
+
+	return MSVCLogs, err
 }
