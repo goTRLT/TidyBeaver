ec6b96a9a21bb3b44eba21768d0a21728306d435
Hursow <thuanraellombardi@gmail.com>
2025-05-30 11:15:39 -0300

    fix: refactor InitS3 function to improve bucket handling and log download prompt

--- internal/storage/s3/s3.go
+++ internal/storage/s3/s3.go
@@ -1,8 +1,22 @@
 package s3
 
+import (
+	"fmt"
+	config "tidybeaver/internal/config"
+)
+
 func InitS3() {
-ListBuckets()
-CreateBucket()
-UploadLogs()
-DownloadLogs()
+	buckets := ListBuckets()
+	if buckets.Buckets == nil {
+		CreateBucket()
+		buckets = ListBuckets()
+	}
+
+	if buckets.Buckets != nil {
+		fmt.Println("Do you wish to download the Previous Logs already stored on your s3 bucket before uploading the New Logs?")
+		if config.CheckAnswer() {
+			DownloadLogs()
+		}
+	}
+	UploadLogs()
 }
