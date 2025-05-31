578f39aa516d629ab164f584ff5992cb5fd49650
Hursow <thuanraellombardi@gmail.com>
2025-05-31 18:51:56 -0300

    refactor: streamline CreateMockedLogs and remove GenerateMockedLogs function

--- internal/sources/mock.go
+++ internal/sources/mock.go
@@ -4,22 +4,44 @@ import (
 	"errors"
 	"log"
 	"math/rand"
+	"strconv"
 	config "tidybeaver/internal/config"
 	models "tidybeaver/pkg/models"
 	"time"
 )
 
-var MockedLogsEntry models.MockedLogs
-var MockedLogEntry models.MockedLog
+var mls models.MockedLogs
+var ml models.MockedLog
 
 func CreateMockedLogs() (model models.MockedLogs, err error) {
-	generatedMockedLogs, err := GenerateMockedLogs()
+	logAmount, _ := strconv.ParseInt(config.CFG.App.LogAmount, 0, 0)
+
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < int(logAmount); i++ {
+		ml, err = GenerateLog()
+		mls.MockedLog = append(mls.MockedLog, ml)
+	}
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return generatedMockedLogs, err
+	return mls, err
+}
+
+func GenerateLog() (ml models.MockedLog, err error) {
+	ml.Level = randomChoice(models.MockedLevels)
+	ml.Service = randomChoice(models.MockedServices)
+	ml.Message = selectMockedMessage(ml.Level)
+	ml.Time = randomTime()
+
+	if ml.Level == "" || ml.Service == "" || ml.Message == "" {
+		err = errors.New("error while setting level, service or message for the Mocked log")
+		log.Fatal(err)
+		return ml, err
+	}
+
+	return ml, err
 }
 
 func randomChoice(choices []string) string {
@@ -41,33 +63,4 @@ func selectMockedMessage(MockedLevel string) string {
 		MockedMessage = randomChoice(models.MockedInfoMessages)
 	}
 	return MockedMessage
-}
-
-func GenerateMockedLogs() (model models.MockedLogs, err error) {
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < config.LogAmountSet; i++ {
-		MockedLogEntry, err = GenerateLog()
-		MockedLogsEntry.MockedLog = append(MockedLogsEntry.MockedLog, MockedLogEntry)
-	}
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return MockedLogsEntry, err
-}
-
-func GenerateLog() (MockedLog models.MockedLog, err error) {
-	MockedLog.Level = randomChoice(models.MockedLevels)
-	MockedLog.Service = randomChoice(models.MockedServices)
-	MockedLog.Message = selectMockedMessage(MockedLog.Level)
-	MockedLog.Time = randomTime()
-
-	if MockedLog.Level == "" || MockedLog.Service == "" || MockedLog.Message == "" {
-		err = errors.New("error while setting level, service or message for the Mocked log")
-		log.Fatal(err)
-		return MockedLog, err
-	}
-
-	return MockedLog, err
 }
