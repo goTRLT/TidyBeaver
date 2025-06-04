704221cb9ebe2bd77abff37cf882139d81be5abe
Hursow <thuanraellombardi@gmail.com>
2025-06-04 18:09:16 -0300

    remove CheckAnswer for s3bucket

--- internal/storage/s3/s3.go
+++ internal/storage/s3/s3.go
@@ -1,10 +1,5 @@
 package s3
 
-import (
-	"fmt"
-	config "tidybeaver/internal/config"
-)
-
 func InitS3() {
 	buckets := ListBuckets()
 	if buckets.Buckets == nil {
@@ -13,10 +8,7 @@ func InitS3() {
 	}
 
 	if buckets.Buckets != nil {
-		fmt.Println("Do you wish to download the Previous Logs already stored on your s3 bucket before uploading the New Logs?")
-		if config.CheckAnswer() {
-			DownloadLogs()
-		}
+		DownloadLogs()
 	}
 	UploadLogs()
 }
