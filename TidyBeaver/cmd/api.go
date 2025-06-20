9bcef456acdf7be7d776a8e6c4a83a936072050f
Hursow <thuanraellombardi@gmail.com>
2025-06-20 15:40:40 -0300

    Refactor InitAPI to retrieve API port from environment variable

--- TidyBeaver/cmd/api.go
+++ TidyBeaver/cmd/api.go
@@ -3,14 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 	api "tidybeaver/internal/api"
 )
 
-const (
-	apiPort = 9090
-)
-
 func InitAPI() {
+	apiPort, err := strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		fmt.Println("Error on getting API Config: ", err)
+	}
+
 	http.HandleFunc("/api/random-response", api.ResponseHandler)
 	fmt.Println("Server running at http://localhost:", apiPort)
 	http.ListenAndServe(fmt.Sprintf(":%d", apiPort), nil)
