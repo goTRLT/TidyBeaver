3984e38a6f3e6428b86eca3f99188390529480bc
Hursow <thuanraellombardi@gmail.com>
2025-06-21 17:02:59 -0300

    Add init-tidybeaverlogs.sql to create user and database for TidyBeaverLogs

---
+++ postgres/initdb/init-tidybeaverlogs.sql
@@ -0,0 +1,12 @@
+CREATE USER tidybeaver WITH PASSWORD 'tidybeaver';
+CREATE DATABASE "TidyBeaverLogs"
+    WITH
+    OWNER = tidybeaver
+    ENCODING = 'UTF8'
+   LC_COLLATE = 'en_US.UTF-8'
+   LC_CTYPE = 'en_US.UTF-8'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+GRANT ALL ON DATABASE "TidyBeaverLogs" TO tidybeaver WITH GRANT OPTION;
