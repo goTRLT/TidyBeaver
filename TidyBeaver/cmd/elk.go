39e3882275bf59031b267365d5e3c889afe0672a
Hursow <thuanraellombardi@gmail.com>
2025-06-25 19:08:35 -0300

    Improve ELK initialization logging and configuration error handling

--- TidyBeaver/cmd/elk.go
+++ TidyBeaver/cmd/elk.go
@@ -6,29 +6,35 @@ import (
 	"os"
 	"strconv"
 	"tidybeaver/internal/elk/router"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func InitElk() {
-
+	log.Println("Initializing ELK")
 	routes := router.NewRouter()
 	elkPort, err := strconv.Atoi(os.Getenv("ELK_PORT"))
 	if err != nil {
-		fmt.Println("Error on getting API Config: ", err)
+		log.Println("Error upon getting ELK Configuration for port: ", err)
+	}
+
+	elkTimeout, err := strconv.Atoi(os.Getenv("ELK_TIMEOUTSECONDS"))
+	if err != nil {
+		log.Println("Error upon getting ELK Configuration for timeout: ", err)
 	}
 
 	server := &http.Server{
 		Addr:           fmt.Sprintf(":%d", elkPort),
 		Handler:        routes,
-		ReadTimeout:    10,
-		WriteTimeout:   10,
+		ReadTimeout:    time.Duration(elkTimeout),
+		WriteTimeout:   time.Duration(elkTimeout),
 		MaxHeaderBytes: 1 << 20,
 	}
 
 	err = server.ListenAndServe()
 	if err != nil {
-		log.Panic(err)
+		log.Println("Error upon creating the ELK Server: ", err)
 	}
-
+	log.Println("ELK initialized")
 }
